f2567ba583988b8d3b48742a33ae589d4f1085f7
Aaron Pankratz <pankratz.aaron@gmail.com>
2019-07-29 23:22:36 -0500

    Updates

---
+++ precache-manifest.48bbe1de5a0fdb4f486d0c7c62ebe931.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9528bfff7714741b06a3436c90efe6aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50ab357506105b463981",
+    "url": "/static/css/main.9093acdf.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "50ab357506105b463981",
+    "url": "/static/js/main.c7056412.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f3208e13b1a58ec028479a798248ed0a",
+    "url": "/static/media/aaron.f3208e13.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.054774adbe886ee6e3c29227ef1745b5.js"
+  "/precache-manifest.48bbe1de5a0fdb4f486d0c7c62ebe931.js"
 );
 
 self.addEventListener('message', (event) => {
